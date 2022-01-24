7be233c34bb87a2f7076d622ba997bc253aeda4e
Qotov <alexander.o.kotov@gmail.com>
2022-01-24 13:08:41 +0100

    files added

---
+++ neuralcrest.py
@@ -0,0 +1,16 @@
+# import
+from jaal import Jaal
+from jaal.datasets import load_got
+import pandas as pd
+
+# load the data
+edge_df = pd.read_csv('nc_grn.csv')
+
+# define vis options
+vis_opts = {'height': '600px', # change height
+            'interaction':{'hover': True}, # turn on-off the hover 
+            'physics':{'stabilization':{'iterations': 100}}} # define the convergence iteration of network
+
+# init Jaal and run server (with gunicorn)
+app = Jaal(edge_df).create()
+server = app.server
